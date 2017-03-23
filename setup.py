667f5060e2ab5e3a885111cecc500d424522e453
Jeremy Banks <_@jeremy.ca>
2017-03-23 17:03:18 -0400

    Automatically build ChatExchange package on GitLab CI.
    Wheel and source distributions (supporting both Python 2 and 3) will be built
    for the master and gl-pages branches. We'll verify that they can each be
    installed and imported (we don't run the full tests again), then include them
    in the gitlab pages push (so you can see information about the build at a URL
    like https://banks.gitlab.io/ChatExchange/dist/wheel.txt).

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import setuptools
 
+
 setuptools.setup(
     name='ChatExchange',
-    version='0.0.3',
+    version='0.0.4.dev0',
     url='https://github.com/Manishearth/ChatExchange',
     packages=[
         'chatexchange'
@@ -11,7 +12,8 @@ setuptools.setup(
         'beautifulsoup4>=4.3.2',
         'requests>=2.2.1',
         'websocket-client>=0.13.0',
-        # only for dev:
+
+        # only required for development:
         'coverage==3.7.1',
         'epydoc>=3.0.1',
         'httmock>=1.2.2',
