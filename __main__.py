a93a55d9234a04548f7adcdd993206032dada79a
Jeremy Banks <_@jeremy.ca>
2017-11-08 03:24:51 -0500

    transcript parser more complete, able to scan back in time

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ import time
 import coloredlogs
 import requests
 
-from chatexchange.parser import *
+from chatexchange.parser import TranscriptPage
 
 
 email = os.environ['ChatExchangeU']
@@ -17,8 +17,20 @@ def main():
     coloredlogs.install(fmt="%(name)s %(levelname)s %(message)s", level=logging.DEBUG)
     logging.getLogger('sqlalchemy').setLevel(logging.DEBUG)
     logging.getLogger('requests').setLevel(logging.DEBUG)
-    print(TranscriptPage(requests.get('https://chat.stackexchange.com/transcript/11540/0-24')))
+    host = 'https://chat.stackexchange.com'
 
+    next_url = host + '/transcript/11540/0-25'
+    while next_url:
+        data = TranscriptPage(requests.get(next_url))
+        previous_day_url = data.previous_day_url or data.first_day_url
+        if previous_day_url:
+            next_url = host + previous_day_url + '/0-24'
+        else:
+            next_url = None
+
+        print(data)
+
+        time.sleep(1)
 
 
 if __name__ == '__main__':
--- chatexchange/parser.py
+++ chatexchange/parser.py
@@ -1,5 +1,11 @@
+"""
+Classes for parsing different HTML pages into structured data.
+
+This performs minimal interpretation, leaving that to the user.
+"""
+
 import re
-from typing import MutableSequence as IList, Optional as Nullable, Union
+from typing import MutableSequence as IList, Optional as Nullable, Union, Tuple
 from html import escape as escape_html
 
 from lxml.etree import ElementBase
@@ -55,13 +61,18 @@ class _ParsedDOM(object):
             # via https://github.com/django-haystack/pysolr/pull/88/files
             sanitized_html = re.sub(u'[^\u0020-\uD7FF\u0009\u000A\u000D\uE000-\uFFFD\U00010000-\U0010FFFF]', '�', html)
             self._dom = self._parser.parse(sanitized_html).getroot()
+
+    @staticmethod
+    def _user_id_and_name_from_link(link_el: ElementBase):
+        user_name = link_el.text
+        user_id = int(link_el.get('href').split('/')[2])
+        return user_id, user_name
     
     __repr__ = _obj_dict.repr
         
 
 
 class TranscriptPage(_ParsedDOM):
-
     class Message(object):
         def __init__(self, **attrs):
             self.id: Nullable[int] = None
@@ -72,7 +83,6 @@ class TranscriptPage(_ParsedDOM):
             self.owner_user_name: Nullable[str] = None
             self.edited: Nullable[bool] = None
             self.parent_message_id: Nullable[int] = None
-            self.time_stamp: Nullable[int] = None
 
             _obj_dict.update(self, **attrs)
 
@@ -80,7 +90,13 @@ class TranscriptPage(_ParsedDOM):
         
     room_id: int
     room_name: str
+
     messages: IList[Message]
+
+    first_day_url: Nullable[str]
+    previous_day_url: Nullable[str]
+    next_day_url: Nullable[str]
+    last_day_url: Nullable[str]
 
     def __init__(self, page):
         super().__init__(page)
@@ -89,14 +105,32 @@ class TranscriptPage(_ParsedDOM):
         self.room_id = int(room_name_link.get('href').split('/')[2])
         self.room_name = room_name_link.text
 
+        self.first_day_url = None
+        self.previous_day_url = None
+        self.next_day_url = None
+        self.last_day_url = None
+
+        for other_day_el in self._dom.cssselect('#main > a[href^="/transcript"]'):
+            if 'first day' in other_day_el.text:
+                self.first_day_url = other_day_el.get('href')
+            elif 'previous day' in other_day_el.text:
+                self.previous_day_url = other_day_el.get('href')
+            elif 'next day' in other_day_el.text:
+                self.next_day_url = other_day_el.get('href')
+            elif 'last day' in other_day_el.text:
+                self.last_day_url = other_day_el.get('href')
+
         self.messages = []
         
         for monologue_el in self._dom.cssselect('.monologue'):
             user_link ,= monologue_el.cssselect('.signature .username a')
+            user_id, user_name = self._user_id_and_name_from_link(user_link)
 
             for message_el in monologue_el.cssselect('.message'):
                 message = TranscriptPage.Message()
 
+                message.owner_user_id = user_id
+                message.owner_user_name = user_name
                 message.id = int(message_el.get('id').split('-')[1])
                 message.edited = bool(message_el.cssselect('.edits'))
                 content_el ,= message_el.cssselect('.content')
